e906ad06850127fd50463b96e350e2d640dfa04d
NguyenTruongGiang <23130082@st.hcmuaf.edu.vn>
2026-07-12 22:19:52 +0700

    style: giao diện trang quản lý vé đã đặt - UC07

--- js/user/trangChu.js
+++ js/user/trangChu.js
@@ -3,6 +3,9 @@ fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
+        if (typeof initHeader === 'function') {
+            initHeader();
+        }
     });
 fetch('footer.html')
     .then(response => response.text())
